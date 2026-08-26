59ec9cbdf7ceff49bfbfcdc974121ed6a1fd613f
Jacobmar2 <jacobmar271@gmail.com>
2026-08-26 08:41:50 -0700

    Fixes for deployment 2

--- static/slideshow.js
+++ static/slideshow.js
@@ -37,10 +37,10 @@
     attribution: '&copy; OpenStreetMap contributors'
   });
 
-  const darkTileLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  const darkTileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    subdomains: 'abcd',
-    attribution: '&copy; OpenStreetMap contributors &copy; CARTO'
+    className: 'slideshow-dark-tiles',
+    attribution: '&copy; OpenStreetMap contributors'
   });
 
   let activeTileTheme = 'light';
